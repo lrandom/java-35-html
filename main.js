b93489dc44efdce71b211e78b51740b4c60e2c2d
lrandom <lrandom@lrandoms-MacBook-Pro.local>
2024-02-28 21:00:06 +0700

    update code

--- main.js
+++
@@ -1,8 +0,0 @@
-const thumbnailItemNode = document.querySelectorAll('.photo-gallery__thumbnail-item')
-thumbnailItemNode.forEach((node) => {
-    node.addEventListener('click', function () {
-        const imgNode = node.querySelector('img');
-        const mainImageNode = document.querySelector('.photo-gallery__main-image img');
-        mainImageNode.setAttribute('src', imgNode.src);
-    });
-});
